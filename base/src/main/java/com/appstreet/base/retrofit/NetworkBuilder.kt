5c41aff22d23e22c5f4ee67113555638a47936ad
harshsidana <harsh.sidana@appstreet.io>
2019-08-04 15:56:37 +0530

    Basic structure

---
+++ base/src/main/java/com/appstreet/base/retrofit/NetworkBuilder.kt
@@ -0,0 +1,42 @@
+package com.appstreet.base.retrofit
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+object NetworkBuilder {
+    const val BASE_URL = "https://github-trending-api.now.sh/"
+
+
+    fun <T> create(
+        baseUrl: String,
+        apiType: Class<T>
+    ) =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(gmHttpClient())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory.invoke())
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(apiType)
+
+    private fun gmHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        val builder = OkHttpClient.Builder()
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .addInterceptor(logging)
+            .retryOnConnectionFailure(true)
+
+
+        return builder.build()
+    }
+
+}
