13df87b57f196fa55eef714f714faad927114ea0
harshsidana <harsh.sidana@appstreet.io>
2019-08-05 16:15:35 +0530

    Image and data caching done

--- base/src/main/java/com/appstreet/base/retrofit/NetworkBuilder.kt
+++ base/src/main/java/com/appstreet/base/retrofit/NetworkBuilder.kt
@@ -1,42 +1,60 @@
 package com.appstreet.base.retrofit
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import okhttp3.Cache
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.util.concurrent.TimeUnit
 
 object NetworkBuilder {
-    const val BASE_URL = "https://github-trending-api.now.sh/"
-
+    val BASE_URL = "https://github-trending-api.now.sh/"
 
     fun <T> create(
         baseUrl: String,
-        apiType: Class<T>
+        apiType: Class<T>,
+        context: Application
     ) =
         Retrofit.Builder()
             .baseUrl(baseUrl)
-            .client(gmHttpClient())
+            .client(gmHttpClient(context))
             .addCallAdapterFactory(CoroutineCallAdapterFactory.invoke())
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
             .create(apiType)
 
-    private fun gmHttpClient(): OkHttpClient {
-        val logging = HttpLoggingInterceptor()
 
-        logging.level = HttpLoggingInterceptor.Level.BODY
-
-        val builder = OkHttpClient.Builder()
-            .connectTimeout(1, TimeUnit.MINUTES)
-            .writeTimeout(1, TimeUnit.MINUTES)
-            .readTimeout(1, TimeUnit.MINUTES)
-            .addInterceptor(logging)
-            .retryOnConnectionFailure(true)
+    private fun gmHttpClient( context: Application): OkHttpClient {
 
 
-        return builder.build()
+        val cacheSize = (5 * 1024 * 1024).toLong()
+        val myCache = Cache(context.cacheDir, cacheSize)
+        val okHttpClient = OkHttpClient.Builder()
+            .cache(myCache)
+            .addInterceptor { chain ->
+                var request = chain.request()
+                request = if (context?.let { hasNetwork(it) }!!)
+                    request.newBuilder().header("Cache-Control", "public, max-age=" + 5).build()
+                else
+                    request.newBuilder().header(
+                        "Cache-Control",
+                        "public, only-if-cached, max-stale=" + 60 * 60 * 24 * 7
+                    ).build()
+                chain.proceed(request)
+            }
+        return okHttpClient.build()
     }
 
+    fun hasNetwork(context: Context): Boolean? {
+        var isConnected: Boolean? = false // Initial Value
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        if (activeNetwork != null && activeNetwork.isConnected)
+            isConnected = true
+        return isConnected
+    }
 }
